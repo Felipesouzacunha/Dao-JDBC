c40ad8ffa3bfa537b297259a07b8808b46d90a8a
Felipe de Souza <131294693+Felipesouzacunha@users.noreply.github.com>
2025-02-05 01:51:17 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,20 @@
 package application;
 
+import java.util.Date;
+
+import model.entities.Department;
+import model.entities.Seller;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Department department = new Department(1, "Books");
+		
+		Seller seller = new Seller(21, "bob", "bob@gmail", new Date(), 3000.0, department);
+		
+		System.out.println(seller);
+		
 	}
 
 }
